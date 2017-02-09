bc29c525d998a6c452b99e14e523d391e474ab3c
Tino Ehrich <tino@bigpun.me>
2017-02-09 16:30:22 +0100

    removed self defined consts in favour of RequestMethodInterface

--- src/Data/RouteData.php
+++ src/Data/RouteData.php
@@ -10,14 +10,6 @@ use Fig\Http\Message\RequestMethodInterface;
  */
 class RouteData implements RequestMethodInterface
 {
-    const METHOD_GET = 'GET';
-    const METHOD_OPTION = 'OPTION';
-    const METHOD_HEAD = 'HEAD';
-    const METHOD_POST = 'POST';
-    const METHOD_PUT = 'PUT';
-    const METHOD_PATCH = 'PATCH';
-    const METHOD_DELETE = 'DELETE';
-
     /**
      * @var string
      */
