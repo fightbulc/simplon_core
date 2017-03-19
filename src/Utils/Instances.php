a595de6bacda0232fc391a172cd4bd2bb2af7cd7
Tino Ehrich <tino@bigpun.me>
2017-03-19 19:32:34 +0100

    use data builder class for instance cache

--- src/Utils/Instances.php
+++ src/Utils/Instances.php
@@ -2,6 +2,8 @@
 
 namespace Simplon\Core\Utils;
 
+use Simplon\Core\Data\InstanceData;
+
 /**
  * @package Simplon\Core\Utils
  */
@@ -13,25 +15,31 @@ class Instances
     private static $cache = [];
 
     /**
-     * @param string $classNamespace
-     * @param array $params
-     * @param callable|null $optional
+     * @param InstanceData $data
      *
      * @return mixed
      */
-    public static function cache(string $classNamespace, array $params = [], ?callable $optional = null)
+    public static function cache(InstanceData $data)
     {
-        if (empty(self::$cache[$classNamespace]))
+        if (empty(self::$cache[$data->getCacheName()]))
         {
-            self::$cache[$classNamespace] = new $classNamespace(...$params);
-
-            if ($optional)
+            if ($data->hasParamsBuilder())
             {
-                self::$cache[$classNamespace] = $optional(self::$cache[$classNamespace]);
+                $builder = $data->getParamsBuilder();
+                $data->setParams($builder());
+            }
+
+            $className = $data->getClassName();
+            self::$cache[$data->getCacheName()] = new $className(...$data->getParams());
+
+            if ($data->hasAfterCallback())
+            {
+                $afterCallback = $data->getAfterCallback();
+                self::$cache[$data->getCacheName()] = $afterCallback(self::$cache[$data->getCacheName()]);
             }
         }
 
-        return self::$cache[$classNamespace];
+        return self::$cache[$data->getCacheName()];
     }
 
 }
